9f16e3bbd794201e349d374a86251fae7667f8b0
Riley Moore <rilemoore@gmail.com>
2019-10-10 00:31:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
   
   if(phrase == phrase.upcase)
     if(phrase == "I LOVE YOU GRANDMA!")
-      puts "I LOVE YOU TOO PUMPKIN!"
+      return "I LOVE YOU TOO PUMPKIN!"
     end
   else
     puts "HUH?! SPEAK UP, SONNY!"
